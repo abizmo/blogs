4966c1febf5cf86ff35f68307dd4c8f9aee6c055
abizmo <abizmo@gmail.com>
2021-06-15 17:50:18 +0100

    Part4 - B - Text

--- src/utils/config.js
+++ src/utils/config.js
@@ -1,7 +1,8 @@
 require('dotenv').config();
 
 const { PORT } = process.env;
-const { DB_URL } = process.env;
+
+const DB_URL = process.env.NODE_ENV === 'test' ? process.env.TEST_DB_URL : process.env.DB_URL;
 
 module.exports = {
   DB_URL,
