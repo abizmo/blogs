726f06d39b1155e618f938d0d885c9f89ca42d01
abizmo <abizmo@gmail.com>
2021-06-15 18:29:40 +0100

    Part4 [6,7]

--- utils/list_helper.js
+++ utils/list_helper.js
@@ -1,14 +1,46 @@
-const dummy = (blogs) => 1;
+const _ = require('lodash');
 
-const totalLikes = (blogs) => blogs.reduce((total, { likes }) => total + likes, 0);
+const dummy = (blogs) => 1;
 
 const favoriteBlog = (blogs) => {
   if (blogs.length === 0) return null;
   return blogs.reduce((prev, curr) => ((prev.likes > curr.likes) ? prev : curr));
 };
 
+const mostBlogs = (blogs) => {
+  if (blogs.length === 0) return null;
+  const blogsByAuthor = _.groupBy(blogs, 'author');
+  const blogsCountByAuthor = [];
+
+  // eslint-disable-next-line no-restricted-syntax
+  for (const [author, b] of Object.entries(blogsByAuthor)) {
+    blogsCountByAuthor.push({ author, blogs: b.length });
+  }
+
+  return _.maxBy(blogsCountByAuthor, 'blogs');
+};
+
+const mostLikes = (blogs) => {
+  if (blogs.length === 0) return null;
+  const blogsByAuthor = _.groupBy(blogs, 'author');
+  const blogsLikesByAuthor = [];
+
+  // eslint-disable-next-line no-restricted-syntax
+  for (const [author, authorBlogs] of Object.entries(blogsByAuthor)) {
+    blogsLikesByAuthor.push({
+      author,
+      likes: authorBlogs.reduce((total, { likes }) => total + likes, 0),
+    });
+  }
+  return _.maxBy(blogsLikesByAuthor, 'likes');
+};
+
+const totalLikes = (blogs) => blogs.reduce((total, { likes }) => total + likes, 0);
+
 module.exports = {
   dummy,
   favoriteBlog,
+  mostBlogs,
+  mostLikes,
   totalLikes,
 };
